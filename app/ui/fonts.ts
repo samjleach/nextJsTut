b03c88b84dd5889e31dc9507fa4736803c0cec7c
samjleach <samleach443@gmail.com>
2025-04-23 17:33:43 +0100

    Initial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from "next/font/google";
+import { Lunasima } from "next/font/google";
+import { Kablammo } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lunasima({ weight: ["400", "700"], style: "normal" });
+
+export const kabalammo = Kablammo({ subsets: ["cyrillic"], weight: ["400"] });
